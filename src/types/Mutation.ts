233b0867cdb2ee31a450eae3c5a9a952dae12e37
Andrei Rivera <andreirive2003@gmail.com>
2022-12-09 19:31:54 -0600

    Boilerplate load V2

---
+++ src/types/Mutation.ts
@@ -0,0 +1,59 @@
+import { APP_SECRET } from '../utils'
+import { compare, hash } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
+import { nonNull, objectType, stringArg } from 'nexus'
+import { Context } from '../context'
+
+export const Mutation = objectType({
+  name: 'Mutation',
+  definition(t) {
+    t.field('signup', {
+      type: 'AuthPayload',
+      args: {
+        name: stringArg(),
+        email: nonNull(stringArg()),
+        password: nonNull(stringArg()),
+      },
+      resolve: async (_parent, args, context: Context) => {
+        const hashedPassword = await hash(args.password, 10)
+        const user = await context.prisma.user.create({
+          data: {
+            name: args.name,
+            email: args.email,
+            password: hashedPassword,
+          },
+        })
+        return {
+          token: sign({ userId: user.id }, APP_SECRET),
+          user,
+        }
+      },
+    })
+
+    t.field('login', {
+      type: 'AuthPayload',
+      args: {
+        email: nonNull(stringArg()),
+        password: nonNull(stringArg()),
+      },
+      resolve: async (_parent, { email, password }, context: Context) => {
+        const user = await context.prisma.user.findUnique({
+          where: {
+            email,
+          },
+        })
+        if (!user) {
+          throw new Error(`No user found for email: ${email}`)
+        }
+        const passwordValid = await compare(password, user.password)
+        if (!passwordValid) {
+          throw new Error('Invalid password')
+        }
+        return {
+          token: sign({ userId: user.id }, APP_SECRET),
+          user,
+        }
+      },
+    })
+  },
+})
